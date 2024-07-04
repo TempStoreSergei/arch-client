dd5c3776a4cea0f562a58ec1ba4141ca863fae1c
sergej <sergawork@gmail.com>
2024-07-04 12:44:26 +0300

    fix: user bug

--- scripts/user.sh
+++ scripts/user.sh
@@ -2,29 +2,31 @@
 
 # Function to create user if it doesn't exist and add to the seat group
 setup_user() {
-    local username="fsuser"
-    
-    if id "$username" &>/dev/null; then
-        info_msg "User '$username' already exists."
+    if id "$USERNAME" &>/dev/null; then
+        info_msg "User '$USERNAME' already exists."
     else
-        info_msg "Creating user '$username'..."
-        if ! echo "$username:admin" | sudo chpasswd; then
-            error_msg "Failed to create user '$username'."
+        info_msg "Creating user '$USERNAME'..."
+        if sudo useradd -m -s /bin/bash "$USERNAME"; then
+            if echo "$USERNAME:$PASSWORD" | sudo chpasswd; then
+                success_msg "User '$USERNAME' created successfully."
+            else
+                error_msg "Failed to set password for user '$USERNAME'."
+                exit 1
+            fi
+        else
+            error_msg "Failed to create user '$USERNAME'."
             exit 1
         fi
-        success_msg "User '$username' created successfully."
     fi
 }
 
 # Function to setup autologin
 setup_autologin() {
-    local username="fsuser"
-    
-    info_msg "Setting up autologin for $username..."
+    info_msg "Setting up autologin for $USERNAME..."
     if ! sudo mkdir -p /etc/systemd/system/getty@tty1.service.d ||
        ! sudo cp conf/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf; then
         error_msg "Failed to setup autologin."
         exit 1
     fi
-    success_msg "Autologin setup for $username successfully."
+    success_msg "Autologin setup for $USERNAME successfully."
 }
